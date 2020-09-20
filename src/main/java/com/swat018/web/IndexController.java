f610643e70c5c6904c4c3c45d063538ef219e83b
jinwoo park <swat018@gmail.com>
2020-09-21 05:38:55 +0900

    mustache - 기본 페이지 만들기 및 테스트

---
+++ src/main/java/com/swat018/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.swat018.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author jinwoopark
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
